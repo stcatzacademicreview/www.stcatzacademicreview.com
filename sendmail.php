91cf87388fcb0de607265c4032bb6fbdd1df4fc1
St Catherine's Academic Review <91825583+stcatzacademicreview@users.noreply.github.com>
2023-02-15 20:22:57 +0530

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+<title>H360_report</title>
+<!-- link -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+<!-- end link -->
+<style>.card{
+    height: 100%;
+    background-color: #ffffff!important;
+    margin: 20px 12px;
+    padding: 10px 25px 20px;
+    height: 100%;
+    box-shadow: 0 3px 6px #00000029;
+    border-radius: 35px;
+    position: relative;
+    text-align: center;
+}
+.card img {
+    max-height: 147px;
+    width: auto;
+}
+
+.card-content h4{
+    color: #434141;
+    font-weight: 900;
+    font-size: 18px;
+    text-transform: capitalize;
+    line-height: 24px;
+    text-align: center;
+}
+
+.card-content p{
+  color: #434141;
+    font-weight: bolder;
+    font-size: 12px;
+    line-height: 17px;
+}
+
+.nb {
+  font-size: 18px;
+    display: inline-block;
+    font-family: Poppins;
+    font-weight: 600;
+    padding: 10px 25px;
+    box-shadow: 6px 7px 16px #00000039;
+    color: #000!important;
+    background-color: #e2e417;
+    border-radius: 5px;
+    border: 0px;
+}
+.vg {
+     font-size: 80px!important;
+    line-height: 77px!important;
+    text-align: center!important;
+    color: #f5fcfb!important;
+font-weight: bolder!important; 
+}</style>
+</head>
+
+<body>
+
+<!-- header start -->
+	
+<section class="container btns">
+<div class="row">
+<div class="col-md-4">
+   <div class="card">
+  <img src="https://livealtlife.com/wp-content/uploads/2022/07/Group-3511.png">
+  <div class="card-content">
+    <h4>Health Restore Kickstarter Silver</h4>
+    <p>A holistic program that focuses on nutrition as well as regular health &amp; risk monitoring &amp; consultations over a 30-day period. The aim is to enable better health outcomes for people with Type-2 Diabetes.</p>
+      <button class="nb"><a href="https://wordpress-844015-3021868.cloudwaysapps.com/shop3/"><span>₹</span>9,999</a></button>
+  </div>
+</div>
+    </div>
+<div class="col-md-4">
+   <div class="card">
+  <img src="https://livealtlife.com/wp-content/uploads/2022/07/Group-3511.png">
+  <div class="card-content">
+    <h4>Health Restore Kickstarter Gold</h4>
+    <p>A holistic program that focuses on nutrition as well as regular health &amp; risk monitoring &amp; consultations over a 30-day period. The aim is to enable better health outcomes for people with Type-2 Diabetes.</p>
+      <button class="nb"><a href="https://wordpress-844015-3021868.cloudwaysapps.com/shop3/"><span>₹</span>9,999</a></button>
+  </div>
+</div>
+    </div>
+<div class="col-md-4">
+   <div class="card">
+  <img src="https://livealtlife.com/wp-content/uploads/2022/07/Group-3511.png">
+  <div class="card-content">
+    <h4>Health Restore Kickstarter Lite (Non-Bangalore)</h4>
+    <p>A holistic program that focuses on nutrition as well as regular health &amp; risk monitoring &amp; consultations over a 30-day period. The aim is to enable better health outcomes for people with Type-2 Diabetes.</p>
+      <button class="nb"><a href="https://wordpress-844015-3021868.cloudwaysapps.com/shop3/"><span>₹</span>9,999</a></button>
+  </div>
+</div>
+    </div>
+<div class="col-md-4">
+   <div class="card">
+  <img src="https://livealtlife.com/wp-content/uploads/2022/07/Group-3511.png">
+  <div class="card-content">
+    <h4>Health Restore Kickstarter Silver</h4>
+    <p>A holistic program that focuses on nutrition as well as regular health &amp; risk monitoring &amp; consultations over a 30-day period. The aim is to enable better health outcomes for people with Type-2 Diabetes.</p>
+      <button class="nb"><a href="https://wordpress-844015-3021868.cloudwaysapps.com/shop3/"><span>₹</span>9,999</a></button>
+  </div>
+</div>
+    </div>
+<div class="col-md-4">
+   <div class="card">
+  <img src="https://livealtlife.com/wp-content/uploads/2022/07/Group-3511.png">
+  <div class="card-content">
+    <h4>Health Restore Kickstarter Lite</h4>
+    <p>A holistic program that focuses on nutrition as well as regular health &amp; risk monitoring &amp; consultations over a 30-day period. The aim is to enable better health outcomes for people with Type-2 Diabetes.</p>
+      <button class="nb"><a href="https://wordpress-844015-3021868.cloudwaysapps.com/shop3/"><span>₹</span>9,999</a></button>
+  </div>
+</div>
+    </div>
+</div>
+</section>
+<!-- ======= FOOTER ======= -->
+
+</body>
+</html>
