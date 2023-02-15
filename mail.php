a5e66f7545f318581174b11044aaacd8ea7bc59a
St Catherine's Academic Review <91825583+stcatzacademicreview@users.noreply.github.com>
2023-02-15 21:37:36 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,15 +1,27 @@
 <?php
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "cnidhi253@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+$to = 'cnidhi253@gmail.com';
+$subject = 'ghkj';
+$from = 'cnidhi253@gmail.com';
+ 
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ 
+// Compose a simple HTML email message
+$message = '<html><body>';
+$message .= '<h1 style="color:#f40;">Hi Jane!</h1>';
+$message .= '<p style="color:#080;font-size:18px;">Will you marry me?</p>';
+$message .= '</body></html>';
+ 
+// Sending email
+if(mail($to, $subject, $message, $headers)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
 }
-//redirect
-header("Location:thankyou.html");
 ?>
